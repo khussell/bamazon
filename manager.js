7ac2e954ee5da862764cf258e9d70a10a8d6c5c7
Lilu <KHussell@gmail.com>
2019-05-29 11:16:05 -0400

    added manager function add new product

--- manager.js
+++ manager.js
@@ -9,42 +9,43 @@ var connection = mysql.createConnection({
     database: 'bamazon_db'
 });
 
-connection.connect(function(err){
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("Connected as id: " + connection.threadId + '\n')
     options();
 });
 
-var options = function(){
+var options = function () {
     inquirer.prompt({
         type: "rawlist",
         message: "What would you like to do?",
         choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
         name: "managerOptions"
-    }).then(function(answer){
-        switch(answer.managerOptions){
+    }).then(function (answer) {
+        switch (answer.managerOptions) {
             case "View Products for Sale":
-                viewProduct()
+                viewProduct();
                 break;
             case "View Low Inventory":
-                viewLowInventory()
+                viewLowInventory();
                 break;
             case "Add to Inventory":
-                addToInventory()
+                addToInventory();
                 break;
             case "Add New Product":
-
+                addNewProduct();
+                break;
         }
     })
 }
 
-var viewProduct = function(){
-    connection.query("SELECT * FROM products", function(err, res){
-        if(err) throw err;
-        var productArr= [['item_id', 'product_name', 'department_name', 'price', 'stock_quantity']];
-        for (var i = 0; i< res.length; i ++){
-         var newProductArr = [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity ];
-         productArr.push(newProductArr)
+var viewProduct = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        var productArr = [['item_id', 'product_name', 'department_name', 'price', 'stock_quantity']];
+        for (var i = 0; i < res.length; i++) {
+            var newProductArr = [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity];
+            productArr.push(newProductArr)
         }
         console.log("\n----------------------------------------------------------------------\n")
         console.log("Bamazon Products: ")
@@ -55,50 +56,81 @@ var viewProduct = function(){
     })
 }
 
-var viewLowInventory = function(){
-    connection.query("SELECT * FROM products WHERE stock_quantity < 15", function(err, res){
-        if(err) throw err;
+var viewLowInventory = function () {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 15", function (err, res) {
+        if (err) throw err;
         console.log("\nItems with low inventory:\n-------------------------")
-        
-        for (var i = 0; i < res.length; i++){
+
+        for (var i = 0; i < res.length; i++) {
             console.log(res[i].product_name)
         }
         connection.end()
     })
 }
 
-var addToInventory = function(){
+var addToInventory = function () {
     inquirer.prompt([{
         message: "What is the id of the product you would like to add?",
         type: "input",
         name: "whatProductID"
-    },{
+    }, {
         message: "What quantity of the product would you like to add?",
         type: "input",
         name: "whatProductQuantity"
-    }]).then(function(answer){
+    }]).then(function (answer) {
         var query = "SELECT * FROM products WHERE products.item_id = " + answer.whatProductID
-        connection.query(query, function(err, res){
-            if(err) throw err;
-           
-           
-                var newQuantity =  res[0].stock_quantity + parseInt(answer.whatProductQuantity);
-                
-                connection.query("UPDATE products SET ? WHERE ?",
+        connection.query(query, function (err, res) {
+            if (err) throw err;
+
+
+            var newQuantity = res[0].stock_quantity + parseInt(answer.whatProductQuantity);
+
+            connection.query("UPDATE products SET ? WHERE ?",
                 [
-                  {
-                    stock_quantity: newQuantity
-                  },
-                  {
-                    item_id: answer.whatProductID 
-                  }
-                ], function(err, response){
+                    {
+                        stock_quantity: newQuantity
+                    },
+                    {
+                        item_id: answer.whatProductID
+                    }
+                ], function (err, response) {
                     if (err) throw err;
                     console.log(response.affectedRows + " product updated!\n")
                     console.log(res[0].product_name + " now has a quantity of " + newQuantity)
                     connection.end()
                 })
-            
+        })
+    })
+}
+
+var addNewProduct = function(){
+    inquirer.prompt([{
+        name: "productToAdd",
+        message: "What product would you like to add?",
+        type: "input"
+    },{
+        name: "department",
+        message: "What department is the product in?",
+        type: "input"
+    },{
+        name: "price",
+        message: "What is the price?",
+        type: "number"
+    },{
+        name: "quantity",
+        message: "What is the quantity?",
+        type: "number"
+    }]).then(function(answer){
+        connection.query("INSERT INTO products SET ?",
+        {
+           product_name: answer.productToAdd,
+           department_name: answer.department,
+           price: answer.price,
+           stock_quantity: answer.quantity 
+        }, function(err, res){
+            if (err) throw err;
+            console.log(res.affectedRows + " new product added!")
+            connection.end();
         })
     })
 }
