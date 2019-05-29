cb54973e4eccf4d92c095dbf1c28e2ea53ace4c7
Lilu <KHussell@gmail.com>
2019-05-29 17:24:05 -0400

    updated bamason.sql for department table and finished readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # bamazon
+
+## What is it?
+
+Bamazon is an Amazon-like storefront that simulates customer, manager, and supervisor actions in the store. This app is used in the command line.
+
+#### Customer Actions:
+
+type:  node customer.js
+
+The customer will be prompted to select from the table of products what product and quantity of the product they would like to purchase. If the quantity is more than what is in stock, the customer will be alerted.
+
+#### Manager Actions:
+
+type: node manager.js
+
+The manager will be prompted to choose to view all the products in a table, view what products are low in stock, add more of a product already in the store, or add a new product to the store.
+
+#### Supervisor Actions:
+
+type: node supervisor.js
+
+The supervisor will be prompted to choose to view all product sales by department or create a new department.
+
+## What you will need?
+
+You will need to clone this repo, set up your own package.json, install npm packages for mysql, inquirer, console.table, and in mysql workbench paste the info in bamazon.sql.
+
+## What was used to make this app?
+
+Javascript, Node, Inquirer NPM, mySql workbench, mySql NPM, and console.table NPM
+
+## Video to see app working:
--- bamazon.sql
+++ bamazon.sql
@@ -8,20 +8,39 @@ product_name VARCHAR(100) NULL,
 department_name VARCHAR(100) NULL,	
 price DECIMAL(10, 2) NULL,
 stock_quantity INTEGER(10),
+product_sales DECIMAL(10,2),
 PRIMARY KEY(item_id)
 );
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Flamingo Pool Float", "Outdoor", 2.00, 10),
-       ("Sunscreen", "Health & Beauty", 8.00, 100),
-	   ("Beach Towel", "Home", 15.00, 40),
-       ("Cooler", "Outdoor", 35.00, 20),
-       ("Watermelon", "Grocery", 5.00, 30),
-       ("Waterproof Speaker", "Electronics", 60.00, 10),
-       ("Beach Umbrella", "Outdoor", 25.00, 20),
-       ("Flip Flops", "Shoes", 5.00, 25),
-       ("Pina Colada Mix", "Grocery", 9.00, 20),
-       ("Rum", "Liquor", 30.00, 15);
+INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Flamingo Pool Float", "Outdoor", 2.00, 10,0),
+       ("Sunscreen", "Health & Beauty", 8.00, 100,0),
+	   ("Beach Towel", "Home", 15.00, 40,0),
+       ("Cooler", "Outdoor", 35.00, 20,0),
+       ("Watermelon", "Grocery", 5.00, 30,0),
+       ("Waterproof Speaker", "Electronics", 60.00, 10,0),
+       ("Beach Umbrella", "Outdoor", 25.00, 20,0),
+       ("Flip Flops", "Shoes", 5.00, 25,0),
+       ("Pina Colada Mix", "Grocery", 9.00, 20,0),
+       ("Rum", "Liquor", 30.00, 15,0);
        
 SELECT * FROM products;
+
+CREATE TABLE departments(
+department_id INTEGER(10) NOT NULL AUTO_INCREMENT,
+department_name VARCHAR(100),
+over_head_costs DECIMAL(10,2),
+PRIMARY KEY(department_id)
+);
+
+INSERT INTO departments(department_name, over_head_costs)
+VALUES("Home", 100.00),
+	   ("Outdoor", 70.00),
+       ("Health & Beauty", 40.00),
+       ("Grocery", 90.00),
+       ("Electronics", 100.00),
+       ("Shoes", 30.00),
+       ("Liquor", 50.00);
+       
+SELECT * FROM departments;
        
